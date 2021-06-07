a3f4b4957119f3271db1ff79d4477daa10e0f04d
mamcdonald94 <mamcdonald94@gmail.com>
2021-06-07 13:42:00 -0400

    inital commit

---
+++ sandbox.py
@@ -0,0 +1,8 @@
+things_to_remember = {
+    "First": "use the 20 minute rule and use the platform and other resources to find my answer",
+    "Second": "ask my classmates for help, like how I would ask a fellow employee at my job",
+    "Third": "ask an available TA in a public setting, so everyone can benefit from my question",
+    "Fourth": "ask an available instructor"
+}
+for num, step in things_to_remember.items():
+    print(num + ", I will " + step)
