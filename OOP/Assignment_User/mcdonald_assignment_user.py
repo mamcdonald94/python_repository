93b4e99bd41617fde4a36176b3efd2a6f56f7acb
mamcdonald94 <mamcdonald94@gmail.com>
2021-06-14 11:09:38 -0400

    updated to chain methods

--- OOP/Assignment_User/mcdonald_assignment_user.py
+++ OOP/Assignment_User/mcdonald_assignment_user.py
@@ -7,18 +7,22 @@ class User:
 
     def make_deposit(self, amount):  # takes an argument that is the amount of the deposit
         self.account_balance += amount  # the specific user's account increases by the amount of the value received
+        return self
 
     def make_withdrawal(self, amount):  # decreases balance by amount specified
         self.account_balance -= amount
+        return self
 
     def transfer_money(self, other_user, amount):
         self.account_balance -= amount
         other_user.account_balance += amount
         print(f"New {other_user.name} balance: ${other_user.account_balance}")
         print(f"New {self.name} balance: ${self.account_balance}")
+        return self
 
     def display_user_balance(self):
         print(f"User: {self.name}, Balance: ${self.account_balance}")
+        return self
 
 
 mark = User("Mark", "fakemail@realmail.com")
@@ -26,22 +30,8 @@ rebekah = User("Rebekah", "realmail@fakemail.com")
 lexi = User("Lexi", "mailfake@fakemail.com")
 
 
-mark.make_deposit(500)
-mark.make_deposit(500)
-mark.make_deposit(500)
-mark.make_withdrawal(850)
-mark.display_user_balance()
+mark.make_deposit(500).make_deposit(500).make_deposit(500).make_withdrawal(850).display_user_balance()
 
-rebekah.make_deposit(2000)
-rebekah.make_deposit(1000)
-rebekah.make_withdrawal(500)
-rebekah.make_withdrawal(1100)
-rebekah.display_user_balance()
+rebekah.make_deposit(2000).make_deposit(1000).make_withdrawal(500).make_withdrawal(1100).display_user_balance()
 
-lexi.make_deposit(5000)
-lexi.make_withdrawal(750)
-lexi.make_withdrawal(50)
-lexi.make_withdrawal(800)
-lexi.display_user_balance()
-
-lexi.transfer_money(mark, 500)
+lexi.make_deposit(5000).make_withdrawal(750).make_withdrawal(50).make_withdrawal(800).display_user_balance().transfer_money(mark, 500)
